161c7bd30de668c237bea88f268534c2d8cb3b00
Tran Thuy <tranthuy240814@gmail.com>
2024-03-09 14:21:33 +0700

    rename worehouse to storage

--- app/Repositories/StorageRepository.php
+++ app/Repositories/StorageRepository.php
@@ -5,37 +5,37 @@ namespace App\Repositories;
 use App\Models\Warehouse;
 use Illuminate\Support\Facades\DB;
 
-class WarehouseRepository extends BaseRepository
+class StorageRepository extends BaseRepository
 
 {
     public function model()
     {
        return Warehouse::class;
     }
-    
+
     public function index()
     {
         return $this->model->orderBy('created_at', 'DESC')->paginate(10);
     }
-    
+
     public function create($input)
     {
         return $this->model->create($input);
     }
-    
+
     public function show($id)
     {
         return $this->model->where('id', $id)->first();
     }
-    
+
     public function update($input, $id)
     {
         return $this->model->where('id', $id)->update($input);
     }
-    
+
     public function destroy($id)
     {
         return $this->model->where('id', $id)->delete();
     }
-    
+
 }
