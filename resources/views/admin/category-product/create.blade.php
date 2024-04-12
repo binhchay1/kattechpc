24395514762fc2db90d9c0e98785f64fa0128313
tranthuy240814 <tranthuy240814@gmail.com>
2024-04-12 14:57:42 +0700

    feature-add-iamge-category

--- resources/views/admin/category-product/create.blade.php
+++ resources/views/admin/category-product/create.blade.php
@@ -18,7 +18,7 @@
                             <label for="categoryInput" class="inline-block mb-2 text-base font-medium">{{ __('Tên danh mục') }}</label>
                             <input type="text" id="categoryInput" name="name" value="{{old('name')}}" class="form-input  border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="{{__('Tên danh mục')}}">
                             @if ($errors->has('name'))
-                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                            <span class="text-danger" style="color: red">{{ $errors->first('name') }}</span>
                             @endif
                         </div>
 
@@ -31,7 +31,29 @@
                                 @endforeach
                             </select>
                         </div>
+                        <div class="xl:col-span-6">
+                            <div class="form-group">
+                                <label for="categorySelect" class="inline-block mb-2 text-base font-medium">{{__('Hình ảnh')}}</label>
+                                <div class="">
+                                    <div class="" style="display: inline-grid;">
+                                        <input value="" type="file" class="border-0 bg-light pl-0" name="image" id="image" hidden>
+                                        <div class=" choose-avatar">
+                                            <div id="btnimage">
+                                                <img id="showImage" class="show-avatar" src="{{ asset('/images/no-image.jpg') }}" alt="avatar"  style="width: 50%; height: auto">
+                                            </div>
+                                            <div id="button">
+                                                <i id="btn_chooseImg" class="fas fa-camera"> {{ __('Chọn hình ảnh') }}</i>
+                                            </div>
+                                        </div>
+                                        @if ($errors->has('image'))
+                                            <span class="text-danger"style="color: red">{{ $errors->first('image') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
                     <div class="flex justify-end gap-2 mt-4">
                         <button type="reset" class="text-red-500 bg-white btn hover:text-red-500 hover:bg-red-100 focus:text-red-500 focus:bg-red-100 active:text-red-500 active:bg-red-100 dark:bg-zink-700 dark:hover:bg-red-500/10 dark:focus:bg-red-500/10 dark:active:bg-red-500/10">{{__('Xóa toàn bộ')}}</button>
                         <button type="submit" class="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">{{__('Thêm danh mục')}}</button>
@@ -42,3 +64,6 @@
     </div>
 </div>
 @endsection
+@push('scripts')
+    <script src="{{ URL::asset('js/admin/eventImage.js') }}"></script>
+@endpush
