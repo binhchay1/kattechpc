63b2852c49115c4e015cc7b7e295d6a34e97d525
binhchay1 <binhchay1@gmail.com>
2024-04-16 13:57:50 +0700

    add post

--- resources/views/page/blog/post-detail.blade.php
+++ resources/views/page/blog/post-detail.blade.php
@@ -1,59 +1,52 @@
 @extends('layouts.page')
 
 @section('title')
-    <title>{{ __('Trang tin tức') }} | Kattech PC</title>
+<title>{{ __('Trang tin tức') }} | Kattech PC</title>
 @endsection
+
 @section('css')
-    <link rel="stylesheet" href="{{ asset('/plugins/owlcarousel/owl.theme.default.min.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('css/page/post-detail.css') }}"/>
+<link rel="stylesheet" href="{{ asset('/plugins/owlcarousel/owl.theme.default.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/page/post-detail.css') }}" />
 @endsection
 
 @section('content')
-<div class="row">
+<div class="row d-flex">
     <div class="leftcolumn">
-        <div class="card">
-            <h1>{{$post->title}}</h1>
-            <p>{{ date_format($post->created_at, "F j, Y, g:i a") }}</p>
-            <div class="" >
-                <img class="boder-radius-10" src="{{$post->thumbnail ?? asset('/images/test_post/2904-viettel-dau-gia-thanh-cong-bang-tan-5g-1.jpg') }}" alt="Viettel trúng đấu giá băng tần để chính thức triển khai 5G tại Việt Nam">
+        <div class="card d-flex">
+            <div class="d-flex flex-direction-column">
+                <h1>{{$post->title}}</h1>
+                <p>{{ date_format($post->created_at, "F j, Y, g:i a") }}</p>
+                <p>{{ $post->short_description }}</p>
             </div>
-            <p>{{$post->short_description}}</p>
-            <div class="row">
-                <p class="">{!!$post->content!!}</p>
 
+            <div>
+                <img class="boder-radius-10" src="{{ $post->thumbnail ?? asset('/images/test_post/2904-viettel-dau-gia-thanh-cong-bang-tan-5g-1.jpg') }}" alt="{{ $post->title }}" width="300" height="300">
             </div>
+        </div>
+
+        <div class="row">
+            <p class="">{!!$post->content!!}</p>
         </div>
     </div>
     <div class="rightcolumn">
         <div class="card">
-
-
-        <h2>{{__("Bài viết liên quan")}}</h2>
-        @foreach($listPost as $post)
-        <div>
+            <h2>{{ __("Bài viết liên quan") }}</h2>
+            @foreach($listPost as $post)
             <div>
-                <img class="image-post" src="{{asset($post->thumbnail)}}">
+                <div>
+                    <img class="image-post" src="{{ asset($post->thumbnail) }}">
+                </div>
+                <a href="{{ route('post.detail', $post['slug']) }}">
+                    <p>{{ $post->title }}</p>
+                </a>
             </div>
-            <a href="{{route('post.detail', $post['slug'])}}">  <p>{{$post->title}}</p></a>
-
-        </div>
-        <br>
+            <br>
             @endforeach
         </div>
     </div>
 </div>
-
 @endsection
 
 @section('js')
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('/plugins/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script>
-        $('.vendor-carousel').owlCarousel({
-            loop: true,
-            nav: false,
-            autoplay: true,
-            smartSpeed: 1000,
-        });
-    </script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
 @endsection
