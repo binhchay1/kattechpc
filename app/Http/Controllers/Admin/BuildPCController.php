f549a68b0934cb9e437618c83eb3ae5070e8a28f
Tran Thuy <tranthuy240814@gmail.com>
2024-04-28 23:24:43 +0700

    category-filter

--- app/Http/Controllers/Admin/BuildPCController.php
+++ app/Http/Controllers/Admin/BuildPCController.php
@@ -14,7 +14,7 @@ class BuildPCController extends Controller
 {
     private $buildPcRepository;
     private $categoryRepository;
-    
+
     public function __construct(
         BuildPcRepository $buildPcRepository,
         CategoryRepository $categoryRepository
@@ -22,31 +22,31 @@ class BuildPCController extends Controller
         $this->buildPcRepository = $buildPcRepository;
         $this->categoryRepository = $categoryRepository;
     }
-    
+
     public function index()
     {
         $listBuildPcs = $this->buildPcRepository->index();
-        
+
         return view('admin.buildPc.index', compact('listBuildPcs'));
     }
-    
+
     public function create()
     {
         $listCategories = $this->categoryRepository->indexOnlyChild();
         return view('admin.buildPc.create', compact('listCategories'));
     }
-    
+
     public function store(BuildPcRequest $request)
     {
         $input = $request->except(['_token']);
-    
+
         $input['category_id'] = json_encode($input['category_id'], true);
-    
+
         $this->buildPcRepository->create($input);
-        
+
         return redirect()->route('admin.buildPC.index')->with('success',  __('Cấu hình được thêm thành công'));
     }
-    
+
     public function edit($id)
     {
         $buildPc = $this->buildPcRepository->show($id);
@@ -54,24 +54,24 @@ class BuildPCController extends Controller
             abort(404);
         }
         $listCategories = $this->categoryRepository->indexOnlyChild();
-    
+
         return view('admin.buildPC.edit', compact('buildPc', 'listCategories'));
     }
-    
-    public function update(Request $request,  $id)
+
+    public function update(BuildPcRequest $request,  $id)
     {
         $input = $request->except(['_token']);
         $input['category_id'] = json_encode($input['category_id'], true);
         $input = $this->buildPcRepository->update($input, $id);
-        
+
         return redirect()->route('admin.buildPC.index')->with('success',  __('Cấu hình được thay đổi thành công'));
     }
-    
-    
+
+
     public function delete($id)
     {
         $this->buildPcRepository->destroy($id);
-        
+
         return back()->with('success', __('Cấu hình  được xóa đổi thành công'));
     }
 }
