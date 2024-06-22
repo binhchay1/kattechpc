2f1fbff500bf78c16d6c062c7d606f4cf7ac377c
binhchay1 <binhchay1@gmail.com>
2024-06-22 10:01:12 +0700

    fix theme

--- resources/views/admin/build-pc/theme.blade.php
+++ resources/views/admin/build-pc/theme.blade.php
@@ -31,7 +31,11 @@
                             <div class="xl:col-span-12">
                                 <label for="editor" class="inline-block mb-2 text-base font-medium">{{__('Nội dung hướng dẫn xây dựng cấu hình')}}
                                 </label>
+                                @if(count($theme) > 0)
                                 <textarea class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" id="editor" name="content" placeholder="{{__('Nội dung xây dựng cấu hình')}}" rows="5">{{ old('description' , $theme[0]->content) }}</textarea>
+                                @else
+                                <textarea class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" id="editor" name="content" placeholder="{{__('Nội dung xây dựng cấu hình')}}" rows="5">{{ old('description') }}</textarea>
+                                @endif
                                 @if ($errors->has('content'))
                                 <span class="text-danger">{{ $errors->first('content') }}</span>
                                 @endif
