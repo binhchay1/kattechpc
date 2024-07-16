b33317bf06debb9ee2ba797d93d07447e4191b28
binhchay1 <binhchay1@gmail.com>
2024-07-17 02:14:34 +0700

    fix post

--- app/Enums/DateLanguage.php
+++ app/Enums/DateLanguage.php
@@ -5,12 +5,12 @@ namespace App\Enums;
 final class DateLanguage
 {
     const Day = [
-        'Mon' => 'Thứ Hai',
-        'Tue' => 'Thứ Ba',
-        'Wed' => 'Thứ Tư',
-        'Thu' => 'Thứ Năm',
-        'Fri' => 'Thứ Sáu',
-        'Sat' => 'Thứ Bảy',
-        'Sun' => 'Chủ Nhật',
+        1 => 'Thứ Hai',
+        2 => 'Thứ Ba',
+        3 => 'Thứ Tư',
+        4 => 'Thứ Năm',
+        5 => 'Thứ Sáu',
+        6 => 'Thứ Bảy',
+        7 => 'Chủ Nhật',
     ];
 }
