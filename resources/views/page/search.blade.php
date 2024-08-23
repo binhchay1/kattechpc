bd421c939d95cac33b8ee6caf6fbf21cabf8e154
binhchay1 <binhchay1@gmail.com>
2024-08-23 10:28:06 +0700

    fix kattech

--- resources/views/page/search.blade.php
+++ resources/views/page/search.blade.php
@@ -30,11 +30,20 @@
     <h1>Chọn theo nhu cầu</h1>
     <div class="row">
         <div class="flex" id="select-price">
-            <div>
+            <div class="area-title-filter">
                 <h2>Lọc theo giá:</h2>
+                <a class="dropbtn" onclick="dropMenuHandle('drop-menu-price')">
+                    <svg class="drop-menu-price-drop-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z" />
+                    </svg>
+
+                    <svg class="drop-menu-price-drop-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M241 130.5l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9l-22.7 22.7c-9.4 9.4-24.5 9.4-33.9 0L224 227.5 69.3 381.5c-9.4 9.3-24.5 9.3-33.9 0l-22.7-22.7c-9.4-9.4-9.4-24.6 0-33.9L207 130.5c9.4-9.4 24.6-9.4 33.9 0z" />
+                    </svg>
+                </a>
             </div>
 
-            <div class="d-flex sort-price-area">
+            <div class="d-flex sort-price-area area-process-in-mobile d-none-in-mobile id=" drop-menu-price"">
                 <div data-id="duoi-10trieu" data-type="price" class="section">
                     <button>{{ __('Dưới 10 triệu') }}</button>
                 </div>
@@ -62,11 +71,20 @@
     </div>
     <div class="row1 mt-1">
         <div class="flex sort">
-            <div>
+            <div class="area-title-filter">
                 <h2>{{ __('Lọc theo tiêu chí:') }}</h2>
+                <a class="dropbtn" onclick="dropMenuHandle('sort')">
+                    <svg class="sort-drop-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z" />
+                    </svg>
+
+                    <svg class="sort-drop-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M241 130.5l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9l-22.7 22.7c-9.4 9.4-24.5 9.4-33.9 0L224 227.5 69.3 381.5c-9.4 9.3-24.5 9.3-33.9 0l-22.7-22.7c-9.4-9.4-9.4-24.6 0-33.9L207 130.5c9.4-9.4 24.6-9.4 33.9 0z" />
+                    </svg>
+                </a>
             </div>
 
-            <div class="flex sort" id="sort" style="flex-wrap: wrap;">
+            <div class="flex sort area-process-in-mobile d-none-in-mobile" id="sort">
                 @if(isset($dataCategory->children))
                 <div class="d-flex flex-direction-column">
                     <label class="font-bold">{{ __('Nhu cầu') }}</label>
@@ -109,8 +127,22 @@
             </div>
         </div>
     </div>
+
     <div class="row2">
-        <div class="flex ">
+        <div class="area-title-filter">
+            <h2>{{ __('Sắp xếp theo:') }}</h2>
+            <a class="dropbtn" onclick="dropMenuHandle('select-sort')">
+                <svg class="select-sort-drop-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z" />
+                </svg>
+
+                <svg class="select-sort-drop-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M241 130.5l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9l-22.7 22.7c-9.4 9.4-24.5 9.4-33.9 0L224 227.5 69.3 381.5c-9.4 9.3-24.5 9.3-33.9 0l-22.7-22.7c-9.4-9.4-9.4-24.6 0-33.9L207 130.5c9.4-9.4 24.6-9.4 33.9 0z" />
+                </svg>
+            </a>
+        </div>
+
+        <div class="flex area-process-in-mobile d-none-in-mobile" id="select-sort">
             <div data-id="new" data-type="sort" class="select button-filter">
                 <button>{{ __('Hàng mới về') }}</button>
             </div>
@@ -124,49 +156,52 @@
                 <button>{{ __('Tên từ A -> Z') }}</button>
             </div>
         </div>
-        <div class="list-product-category list-product-flex d-flex flex-wrap gap-12">
-            @foreach($listProducts as $product)
-            <div class="product-item">
-                <a href="{{ route('productDetail', $product['slug']) }}" class="product-image">
-                    <img width="200" height="200" alt="{{ $product->name }}" class="lazy" src="{{ asset(json_decode($product->image, true)[0]) }}">
-                    <span class="p-type-holder">
-                        @if($product->hot_status)
-                        <i class="p-icon-type p-icon-hot"></i>
-                        @endif
-                    </span>
-                    <span class="p-type-holder p-type-holder-2">
-                        @if($product->hot_sale_status)
-                        <i class="p-icon-type p-icon-best-sale"></i>
-                        @endif
-                    </span>
-                </a>
 
-                <div class="product-info flex-1">
-                    <a href="{{ route('productDetail', $product['slug']) }}">
-                        <h3 class="product-title line-clamp-3">{{ $product->name }}</h3>
-                    </a>
-                    @if($product->new_price != null)
-                    <div class="product-martket-main d-flex align-items-center">
-                        <p class="product-market-price">{{ $product->price }} đ</p>
-                        <?php $new_price = floor(100 - (((int) $product->new_price / (int) $product->price) * 100)) ?>
-                        <div class="product-percent-price">
-                            -{{ ($new_price) }} %
-                        </div>
-                    </div>
-
-                    <div class="product-price-main font-weight-600">
-                        {{ $product->new_price }} đ
-                    </div>
-                    @else
-                    <div class="product-price-main font-weight-600">
-                        {{ $product->price }} đ
-                    </div>
-                    @endif
-                </div>
-            </div>
-            @endforeach
-        </div>
     </div>
+
+    <div class="list-product-category list-product-flex d-flex flex-wrap gap-12">
+        @foreach($listProducts as $product)
+        <div class="product-item">
+            <a href="{{ route('productDetail', $product['slug']) }}" class="product-image">
+                <img width="200" height="200" alt="{{ $product->name }}" class="lazy" src="{{ asset(json_decode($product->image, true)[0]) }}">
+                <span class="p-type-holder">
+                    @if($product->hot_status)
+                    <i class="p-icon-type p-icon-hot"></i>
+                    @endif
+                </span>
+                <span class="p-type-holder p-type-holder-2">
+                    @if($product->hot_sale_status)
+                    <i class="p-icon-type p-icon-best-sale"></i>
+                    @endif
+                </span>
+            </a>
+
+            <div class="product-info flex-1">
+                <a href="{{ route('productDetail', $product['slug']) }}">
+                    <h3 class="product-title line-clamp-3">{{ $product->name }}</h3>
+                </a>
+                @if($product->new_price != null)
+                <div class="product-martket-main d-flex align-items-center">
+                    <p class="product-market-price">{{ $product->price }} đ</p>
+                    <?php $new_price = floor(100 - (((int) $product->new_price / (int) $product->price) * 100)) ?>
+                    <div class="product-percent-price">
+                        -{{ ($new_price) }} %
+                    </div>
+                </div>
+
+                <div class="product-price-main font-weight-600">
+                    {{ $product->new_price }} đ
+                </div>
+                @else
+                <div class="product-price-main font-weight-600">
+                    {{ $product->price }} đ
+                </div>
+                @endif
+            </div>
+        </div>
+        @endforeach
+    </div>
+
     <div class="center">
         <div class="pagination">
             {{ $listProducts->links('page.paginate.default') }}
@@ -177,46 +212,5 @@
 
 @section('js')
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-<script>
-    var url = location.href;
-    var arrayParam = [];
-
-    if (url.indexOf('?') != -1) {
-        var ary = url.split('?')[1].split('&');
-        for (i = 0; i <= ary.length - 1; i++) {
-            var val = ary[i].split('=')[1];
-            var param = ary[i].split('=')[0];
-            arrayParam[param] = val;
-        }
-    }
-
-    $(document).ready(function() {
-        $('#select-price div').click(function(event) {
-            event.preventDefault();
-            let type = $(this).data('type');
-            let id = $(this).data('id');
-            let url = location.protocol + '//' + location.host + location.pathname;
-
-            if (type == 'price') {
-                arrayParam['price'] = id;
-            }
-
-            if (type == 'sort') {
-                arrayParam['sort'] = id;
-            }
-
-            for (var k in arrayParam) {
-                if (arrayParam.hasOwnProperty(k)) {
-                    if (k == Object.keys(arrayParam)[0]) {
-                        url += '?' + k + '=' + arrayParam[k];
-                    } else {
-                        url += '&' + k + '=' + arrayParam[k];
-                    }
-                }
-            }
-
-            window.location.href = url;
-        });
-    });
-</script>
+<script src="{{ asset('/js/page/product-category.js') }}"></script>
 @endsection
