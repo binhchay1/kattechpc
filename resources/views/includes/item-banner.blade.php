aa8ce91f17a57fdf0fc7c513cfc4293aa52cbd35
binhchay1 <binhchay1@gmail.com>
2024-10-25 10:43:28 +0700

    change item

--- resources/views/includes/item-banner.blade.php
+++ resources/views/includes/item-banner.blade.php
@@ -2,7 +2,7 @@
 @if($layout->hide_small_banner_thumbnail_left == 0)
 <div class="fixed-left">
     <a target="_blank" href="{{ isset($layout->permarklink_small_banner_thumbnail_left) ? $layout->permarklink_small_banner_thumbnail_left : '#' }}">
-        <img width="250" height="450" alt="banner-left" src="{{ asset($layout->small_banner_thumbnail_left) }}">
+        <img width="250" height="700" alt="banner-left" src="{{ asset($layout->small_banner_thumbnail_left) }}">
     </a>
 </div>
 @endif
@@ -12,7 +12,7 @@
 @if($layout->hide_small_banner_thumbnail_right == 0)
 <div class="fixed-right">
     <a target="_blank" href="{{ isset($layout->permarklink_small_banner_thumbnail_right) ? $layout->permarklink_small_banner_thumbnail_right : '#' }}">
-        <img width="250" height="450" alt="banner-right" src="{{ asset($layout->small_banner_thumbnail_right) }}">
+        <img width="250" height="700" alt="banner-right" src="{{ asset($layout->small_banner_thumbnail_right) }}">
     </a>
 </div>
 @endif
