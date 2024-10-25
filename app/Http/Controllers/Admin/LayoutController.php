10f866a337139035cda2dc4dd3c6c397b7e3816d
binhchay1 <binhchay1@gmail.com>
2024-10-25 09:03:25 +0700

    fix add store slide

--- app/Http/Controllers/Admin/LayoutController.php
+++ app/Http/Controllers/Admin/LayoutController.php
@@ -119,7 +119,6 @@ class LayoutController extends Controller
 
     public function storeSlide(Request $request)
     {
-        dd($request);
         $input = $request->except(['_token']);
         if (isset($input['slide_image'])) {
             $input['slide_image']->move(public_path('images/upload/layout/'), $input['slide_image']->getClientOriginalName());
