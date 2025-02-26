2c48dd49b199fe68fd9b847456bac9cba1f356c4
binhchay1 <binhchay1@gmail.com>
2025-02-27 00:15:55 +0700

    fix all

--- database/seeders/UsersDB.php
+++ database/seeders/UsersDB.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersDB extends Seeder
 {
